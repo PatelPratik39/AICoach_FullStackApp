9bba7a54649fb8bba756f3178af44664e9ac4dfd
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-11 20:15:23 -0600

    Vector Database is crated now

---
+++ AiCoach_FullStack/src/main/java/dev/aicoach/AiCoachfullstack/AiCoachFullStackApplication.java
@@ -0,0 +1,14 @@
+package dev.aicoach.AiCoachfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoachFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCoachFullStackApplication.class, args);
+		System.out.println("AiCoachFullStackApplication started Happily 🤗");
+	}
+
+}
